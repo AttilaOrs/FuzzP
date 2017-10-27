2a845135ec852d6ec68fec8f9947241e83a56637
AttilaOrs <kilyen.attila.ors@gmail.com>
2017-10-27 12:04:24 +0300

    bigger pop

--- FuzzPGenetic/src/UnifiedGpProblmes/Robo/Main.java
+++ FuzzPGenetic/src/UnifiedGpProblmes/Robo/Main.java
@@ -93,7 +93,7 @@ public class Main {
     MultiobjectiveMulioperatorGA<UnifiedGpIndi> algo = new MultiobjectiveMulioperatorGA<>(pool, otherSelector,
         survSelector, null, new double[] { 1.0 }, new double[] { 1.0 }, crossWeigth, new double[] { 1.0 });
     SimpleGA.iteration = 150;
-    SimpleGA.population = 3000;
+    SimpleGA.population = 6000;
     long start = System.currentTimeMillis();
     algo.theAlgo();
     long stop = System.currentTimeMillis();
