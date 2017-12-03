3914c19d85accd37e3587c7bb83dee527793e4e6
AttilaOrs <kilyen.attila.ors@gmail.com>
2017-12-03 16:18:54 +0200

    Main

--- FuzzPGenetic/src/UnifiedPetriRuleOptimizer/PiFitness/PiMain.java
+++ FuzzPGenetic/src/UnifiedPetriRuleOptimizer/PiFitness/PiMain.java
@@ -26,42 +26,41 @@ import structure.operators.ICreatureMutator;
 public class PiMain {
 
   public static void main(String[] args) {
-    for(int tryning = 0; tryning < 100; tryning++) {
+    for (int tryning = 0; tryning < 100; tryning++) {
 
-    PiUnifiedPetriMaker maker = new PiUnifiedPetriMaker();
-    BitIndiDecoder decoder = new BitIndiDecoder(maker.net,
-        IntStream.range(0, maker.net.getNrOfTransition()).mapToObj(t -> t).collect(toList()));
-    System.out.println("nr of rules:: " + decoder.getNrOfRules());
+      PiUnifiedPetriMaker maker = new PiUnifiedPetriMaker();
+      BitIndiDecoder decoder = new BitIndiDecoder(maker.net,
+          IntStream.range(0, maker.net.getNrOfTransition()).mapToObj(t -> t).collect(toList()));
+      System.out.println("nr of rules:: " + decoder.getNrOfRules());
 
-    ArrayList<IOperatorFactory<ICreatureGenerator<BitIndi>>> gens = new ArrayList<>();
-    gens.add(() -> new Generator(decoder.getNrOfRules()));
+      ArrayList<IOperatorFactory<ICreatureGenerator<BitIndi>>> gens = new ArrayList<>();
+      gens.add(() -> new Generator(decoder.getNrOfRules()));
 
-    ArrayList<IOperatorFactory<ICreatureMutator<BitIndi>>> mutators = new ArrayList<>();
-    mutators.add(Mutator::new);
+      ArrayList<IOperatorFactory<ICreatureMutator<BitIndi>>> mutators = new ArrayList<>();
+      mutators.add(Mutator::new);
 
-    ArrayList<IOperatorFactory<ICreatureBreeder<BitIndi>>> breeders = new ArrayList<>();
-    breeders.add(UnifromCrossoverBitIndi::new);
+      ArrayList<IOperatorFactory<ICreatureBreeder<BitIndi>>> breeders = new ArrayList<>();
+      breeders.add(UnifromCrossoverBitIndi::new);
 
-    ArrayList<IOperatorFactory<ICreatureFitnes<BitIndi>>> fitnesses = new ArrayList<>();
-    fitnesses.add(FirstOrderFitnes::new);
+      ArrayList<IOperatorFactory<ICreatureFitnes<BitIndi>>> fitnesses = new ArrayList<>();
+      fitnesses.add(FirstOrderFitnes::new);
 
-    ICreaturePool<BitIndi> pool = new CreaturePoolWithStreams<>(gens, mutators, breeders, fitnesses);
+      ICreaturePool<BitIndi> pool = new CreaturePoolWithStreams<>(gens, mutators, breeders, fitnesses);
 
-    SimpleGA<BitIndi> algo = new SimpleGA<>(pool, new LinearRankSelection());
-    SimpleGA.iteration = 100;
-    SimpleGA.population = 2000;
-    algo.theAlgo();
-    Integer best = algo.getMaxId();
-    BitIndi i = pool.get(best);
-    UnifiedPetriNet newNEt = decoder.modified(i);
-    PetriNetJsonSaver<UnifiedPetriNet> saver = new PetriNetJsonSaver<UnifiedPetriNet>();
-    String jsonStr = saver.makeString(newNEt);
+      SimpleGA<BitIndi> algo = new SimpleGA<>(pool, new LinearRankSelection());
+      SimpleGA.iteration = 100;
+      SimpleGA.population = 2000;
+      algo.theAlgo();
+      Integer best = algo.getMaxId();
+      BitIndi i = pool.get(best);
+      UnifiedPetriNet newNEt = decoder.modified(i);
+      PetriNetJsonSaver<UnifiedPetriNet> saver = new PetriNetJsonSaver<UnifiedPetriNet>();
+      String jsonStr = saver.makeString(newNEt);
       PlotUtils.writeToFile("piRez_n" + tryning + ".json", jsonStr);
-    FirstOrderFitnes fitnes = new FirstOrderFitnes();
-    double rez = fitnes.evaluate(i);
+      FirstOrderFitnes fitnes = new FirstOrderFitnes();
+      double rez = fitnes.evaluate(i);
       PlotUtils.writeToFile("rez" + tryning + ".txt", Double.toString(rez));
     }
-
 
   }
 
