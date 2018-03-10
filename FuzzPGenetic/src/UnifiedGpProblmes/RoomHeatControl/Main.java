bcfa63435390b5500e9b42ffd5eacdf789a1e3b0
AttilaOrs <kilyen.attila.ors@gmail.com>
2018-03-10 21:46:42 +0100

    bigger pupulation

--- FuzzPGenetic/src/UnifiedGpProblmes/RoomHeatControl/Main.java
+++ FuzzPGenetic/src/UnifiedGpProblmes/RoomHeatControl/Main.java
@@ -98,7 +98,7 @@ public class Main {
       MultiobjectiveMulioperatorGA<UnifiedGpIndi> algo = new MultiobjectiveMulioperatorGA<>(pool, otherSelector,
           survSelector, null, new double[] { 1.0 }, new double[] { 1.0 }, crossWeigth, new double[] { 1.0 });
       SimpleGA.iteration = 100;
-      SimpleGA.population = 3000;
+      SimpleGA.population = 5000;
       algo.setEralyStoppingCondition(d -> d >= 1.0);
       long start = System.currentTimeMillis();
       algo.theAlgo();
